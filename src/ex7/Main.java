eff97eb68eb1d81b72d8ebc40b1590b9b4a6640e
hutch28 <hutchinsjo@gcc.edu>
2018-11-09 10:47:49 -0500

    make conflict

--- src/ex7/Main.java
+++ src/ex7/Main.java
@@ -3,11 +3,11 @@ package ex7;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git johutchi");
 	}
 	
 	public static void yell() {
-		System.out.println("HELLO GIT");
+		System.out.println("HELLO GIT YELL2");
 	}
 
 }
