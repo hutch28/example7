301e1e97a789496c735658a7b5871d4439435394
hutch28 <hutchinsjo@gcc.edu>
2018-11-09 10:49:42 -0500

    rr

--- src/ex7/Main.java
+++ src/ex7/Main.java
@@ -3,7 +3,7 @@ package ex7;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git johutchi");
+		System.out.println("Helrrrlo Git johutchi");
 	}
 	
 	public static void yell() {
