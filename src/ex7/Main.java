0151858065ea7f4e19afb8800577f1e4fafd485d
hutch28 <hutchinsjo@gcc.edu>
2018-11-09 09:59:10 -0500

    added main class

---
+++ src/ex7/Main.java
@@ -0,0 +1,13 @@
+package ex7;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+	
+	public static void yell() {
+		System.out.println("HELLO GIT");
+	}
+
+}
